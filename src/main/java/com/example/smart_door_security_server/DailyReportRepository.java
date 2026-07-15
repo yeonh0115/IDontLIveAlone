acae96980f8a354e3021ce552fd3e1f1d045d4c2
yeonh0115 <rladusgml0115@gmail.com>
2026-07-15 12:22:39 +0900

    Update DailyReportRepository.java

--- src/main/java/com/example/smart_door_security_server/DailyReportRepository.java
+++ src/main/java/com/example/smart_door_security_server/DailyReportRepository.java
@@ -3,9 +3,14 @@ package com.example.smart_door_security_server;
 import com.example.smart_door_security_server.DailyReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface DailyReportRepository extends JpaRepository<DailyReport, Integer> {
     @Query("SELECT d FROM DailyReport d ORDER BY d.reportDate DESC")
     List<DailyReport> findAllReports();
+
+    // 📌 [추가] 특정 유저와 특정 날짜로 리포트를 찾아주는 기능
+    Optional<DailyReport> findByUserAndReportDate(User user, LocalDate reportDate);
 }
